61707ecae06ebd0b7c0ab199c17910cb98b45b06
Jonathan Frederick <jrfreder@gmail.com>
2015-09-10 18:04:13 -0400

    commented things out, changed list comps to dict comps

--- movie_lib.py
+++ movie_lib.py
@@ -2,37 +2,51 @@
 
 class User:
 
-    def __init__(self, ident, user_age, user_gender, user_occupation, zip_code):
+    def __init__(self, ident, **kwargs):
         self.user_id = ident
-        self.age = user_age
-        self.gender = user_gender
-        self.occupation = user_occupation
+#        self.age = user_age
+#        self.gender = user_gender
+#        self.occupation = user_occupation
+        self.user_ratings = {}
+
+    def get_ratings(rating_list):
+        self.user_ratings = {rat[1] : rat[2] for rat in rating_list if rat[0] == self.user_id}
 
 
 class Rating:
 
-    def __init__(self, rating_user, rating_item, rating_, rating_timestamp):
+    def __init__(self, rating_, rating_user=None, rating_item=None, **kwargs):
         self.user_id = rating_user
         self.item_id = rating_item
         self.rating = rating_
-        self.timestamp = rating_timestamp
+#        self.timestamp = rating_timestamp
 
 
 class Movie:
 
-    def __init__(self, movie_id, movie_title, release_date, video_release_date, imdb_url, *args):
+    def __init__(self, movie_id, **kwargs):
         self.item_id = movie_id
         self.title = movie_title
-        self.release = release_date
-        self.vid_release = video_release_date
-        self.url = imdb_url
-        self.genres = [*args]
-        self.genre_names = ['unknown', 'Action', 'Adventure', 'Animation',
-                            'Children\'s', 'Comedy', 'Crime', 'Documentary',
-                            'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'Musical',
-                            'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
+#        self.release = release_date
+#        self.vid_release = video_release_date
+#        self.url = imdb_url
+#        self.genres = [*args]
+#        self.genre_names = ['unknown', 'Action', 'Adventure', 'Animation',
+#                            'Children\'s', 'Comedy', 'Crime', 'Documentary',
+#                            'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'Musical',
+#                            'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
+        self.movie_ratings = []
+        self.avg_ratings = None
 
+    def get_ratings(rating_list):
+        self.movie_ratings = {rat[0] : rat[2] for rat in rating_list if rat[1] == self.movie_id}
 
+    def avg_ratings(rating_list):
+        self.avg_ratings = sum(get_ratings(rating_list))/len(self.movie_ratings)
+
+    def name_for_id(ident):
+        if self.item_id == ident:
+            return self.title
 
 
 
