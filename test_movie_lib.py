ff04bac835fad29a94d721c2e7167a370118ae81
Jonathan Frederick <jrfreder@gmail.com>
2015-09-10 22:04:57 -0400

    implemented test_movie_init

--- test_movie_lib.py
+++ test_movie_lib.py
@@ -1,7 +1,7 @@
 from movie_lib import *
 
 users = {123:User(234), 345:User(456), 567:User(678)}
-
+movies = {543:Movie(876, "A Bug's Life"), 321:Movie(987, 'Toy Story'), 432:Movie(654, 'The Incredibles')}
            #usr, mov, rating
 ratings = [[234, 987, 3],
            [234, 876, 4],
@@ -34,3 +34,11 @@ def test_user_get_ratings():
     # assert users[567].user_ratings == {987: Rating(5), 654: Rating(3)}
 
 def test_movie_init():
+    assert movies[543].item_id == 876
+    assert movies[543].title == "A Bug's Life"
+    assert movies[321].item_id == 987
+    assert movies[321].title == "Toy Story"
+    assert movies[432].item_id == 654
+    assert movies[432].title == "The Incredibles"
+    assert movies[432].item_id != 135
+    assert movies[432].title != "Cars"
